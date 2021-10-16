522aa71b580e96cf244b25533bda354796dd6cab
samuel-cavalcanti <scavalcanti111@gmail.com>
2021-10-16 20:37:14 -0300

    protocol não precisa de abstract method

--- src/ufu_scanner/token_scan.py
+++ src/ufu_scanner/token_scan.py
@@ -1,13 +1,9 @@
 from src.ufu_token.ufu_token import UfuToken
 from src.source_program.source_program import SourceProgram
 from typing import Protocol, Optional
-from abc import abstractmethod
-
-
 
 
 class TokenScan(Protocol):
 
-    @abstractmethod
     def scan(self, source: SourceProgram) -> Optional[UfuToken]:
-        raise NotImplementedError()
+        ...
